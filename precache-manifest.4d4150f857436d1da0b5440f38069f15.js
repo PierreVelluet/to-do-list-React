0b463426a572540bb9241f50dbf5f53b3437f8a9
Pierre Velluet <velluetp@gmail.com>
2020-01-22 01:10:14 +0100

    Updates

---
+++ precache-manifest.4d4150f857436d1da0b5440f38069f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21e9005dfa36ed72acb6f136073d550",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "9546be1c15543208bc34",
+    "url": "/to-do-list-react/static/css/main.7af3dcfd.chunk.css"
+  },
+  {
+    "revision": "26855a0d47e911866a79",
+    "url": "/to-do-list-react/static/js/2.5ee41035.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-list-react/static/js/2.5ee41035.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9546be1c15543208bc34",
+    "url": "/to-do-list-react/static/js/main.365a8ff8.chunk.js"
+  },
+  {
+    "revision": "8918f33dde8633255aca",
+    "url": "/to-do-list-react/static/js/runtime-main.3bd68460.js"
+  }
+]);
